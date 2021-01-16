ce9202945e3b0a81d0d19f116f366dba7e5bd74b
tokkk <mease250@gmail.com>
2021-01-16 19:59:35 +0900

    change

--- js/toglle.js
+++ js/toglle.js
@@ -1,11 +1,11 @@
 $(function() {
-  $('../header.html#nav-toggle').click(function() {
+  $('#nav-toggle').click(function() {
    $(this).toggleClass('active');
   $('.menu').toggleClass('open');
  });
     $('.menu a').on('click', function(){
         if (window.innerWidth <= 768) {
-            $('../header.html#nav-toggle').click();
+            $('#nav-toggle').click();
         }
     });
 });
